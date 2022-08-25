3fa08e37cc2ebd0e4b8fa9ffb32e993062533ff3
Ralf Silva <ralf.velani@gmail.com>
2022-08-25 14:59:58 -0300

    Creating abstraction project

---
+++ Aula04/src/main/java/com/mycompany/aula04/Aula04.java
@@ -0,0 +1,12 @@
+package com.mycompany.aula04;
+
+public class Aula04 {
+
+    public static void main(String[] args) {
+        
+        Caneta c1 = new Caneta("Bic Cristal", "Azul", 0.6f);
+        c1.status();
+        
+        
+    }
+}
