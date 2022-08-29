a88ddd8844babe17e0d0c509c7ea1e2589590bf6
Ralf Silva <ralf.velani@gmail.com>
2022-08-28 21:43:47 -0300

    Encapsulation design with interface implementation

---
+++ encapsulamento/src/main/java/com/mycompany/encapsulamento/Encapsulamento.java
@@ -0,0 +1,24 @@
+package com.mycompany.encapsulamento;
+
+public class Encapsulamento {
+
+    public static void main(String[] args) {
+        
+        ControleRemoto cr1 = new ControleRemoto();
+        cr1.ligar();
+        
+        cr1.play();
+        cr1.abrirMenu();
+        cr1.maisVolume();
+        cr1.maisVolume();cr1.maisVolume();
+        cr1.maisVolume();cr1.maisVolume();
+        cr1.maisVolume();cr1.maisVolume();
+        cr1.maisVolume();cr1.maisVolume();
+        cr1.maisVolume();cr1.maisVolume();
+        cr1.maisVolume();
+        
+        cr1.abrirMenu();
+        
+                    
+    }
+}
