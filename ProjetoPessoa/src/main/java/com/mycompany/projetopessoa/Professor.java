255a4278b65bcf222041b8036b9e62eafaa39449
Ralf Silva <ralf.velani@gmail.com>
2022-09-05 15:32:06 -0300

    Class Professor extends Pessoa

---
+++ ProjetoPessoa/src/main/java/com/mycompany/projetopessoa/Professor.java
@@ -0,0 +1,32 @@
+package com.mycompany.projetopessoa;
+
+public class Professor extends Pessoa {
+    
+    private String especialidade;
+    private float salario;
+    
+    public void receberAum(float aumento) {
+        float aum = aumento;
+        this.setSalario(getSalario() + aumento);
+        System.out.println(this.getNome() +  " o seu salário atual é: " + this.getSalario() + ", o anterior era: " + aum);
+    }
+
+    public Professor() {
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+}
