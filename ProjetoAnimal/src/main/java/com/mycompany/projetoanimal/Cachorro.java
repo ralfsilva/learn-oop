eab62ee8cf929dbca41f97170871b7aad7d1d101
Ralf Silva <ralf.velani@gmail.com>
2022-09-08 14:34:37 -0300

    Cachorro class extends Mamifero - Override inheritance

---
+++ ProjetoAnimal/src/main/java/com/mycompany/projetoanimal/Cachorro.java
@@ -0,0 +1,9 @@
+package com.mycompany.projetoanimal;
+
+public class Cachorro extends Mamifero {
+
+    @Override
+    public void emitirSom() {
+        System.out.println("au au au");
+    }      
+}
