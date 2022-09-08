ac42dd5bbbe5cb1a8f0c1790085b9828dd3288f6
Ralf Silva <ralf.velani@gmail.com>
2022-09-08 14:40:42 -0300

    Tartaruga class extends Reptil - Override inheritance

---
+++ ProjetoAnimal/src/main/java/com/mycompany/projetoanimal/Tartaruga.java
@@ -0,0 +1,10 @@
+package com.mycompany.projetoanimal;
+
+public class Tartaruga extends Reptil {
+
+    @Override
+    public void locomover() {
+        System.out.println("Caminhando devagar");
+    }
+    
+}
