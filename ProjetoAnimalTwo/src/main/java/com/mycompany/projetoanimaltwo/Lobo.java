06adbd15dc422be68b63de7d43c1a37fc2891b7f
Ralf Silva <ralf.velani@gmail.com>
2022-09-09 14:45:58 -0300

    Lobo class extends Mamifero

---
+++ ProjetoAnimalTwo/src/main/java/com/mycompany/projetoanimaltwo/Lobo.java
@@ -0,0 +1,9 @@
+package com.mycompany.projetoanimaltwo;
+
+public class Lobo extends Mamifero {
+
+    @Override
+    public void emitirSom() {
+        System.out.println("Auuuuuuuuuuuuu !");
+    } 
+}
