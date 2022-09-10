68639e89360792a385bacd7149e6868b5627087e
Ralf Silva <ralf.velani@gmail.com>
2022-09-10 15:52:07 -0300

    Class Arara extends Ave

---
+++ ProjetoAnimal/src/main/java/com/mycompany/projetoanimal/Arara.java
@@ -0,0 +1,35 @@
+package com.mycompany.projetoanimal;
+
+public class Arara extends Ave {
+
+    private String tipoArara;
+    private String tamanho;
+    private String tempoDeVida;
+
+    public String getTipoArara() {
+        return tipoArara;
+    }
+
+    public void setTipoArara(String tipoArara) {
+        this.tipoArara = tipoArara;
+    }
+
+    public String getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(String tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public String getTempoDeVida() {
+        return tempoDeVida;
+    }
+
+    public void setTempoDeVida(String tempoDeVida) {
+        this.tempoDeVida = tempoDeVida;
+    }
+    
+    
+    
+}
