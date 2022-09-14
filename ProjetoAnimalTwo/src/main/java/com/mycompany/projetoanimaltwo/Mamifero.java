c900be33bdcbd72c27a3f12091fcdfd03104b7e0
Ralf Silva <ralf.velani@gmail.com>
2022-09-14 15:26:46 -0300

    Mamifero class extends Animal

---
+++ ProjetoAnimalTwo/src/main/java/com/mycompany/projetoanimaltwo/Mamifero.java
@@ -0,0 +1,12 @@
+package com.mycompany.projetoanimaltwo;
+
+public class Mamifero extends Animal {
+
+    protected String corPelo;
+
+    @Override
+    public void emitirSom() {
+        System.out.println("Som de mamifero");
+    }
+    
+}
